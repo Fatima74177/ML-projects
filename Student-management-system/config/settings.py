fb5027019789266760474065b2eee4f7734da415
Fatima74177 <laibaak081@gmail.com>
2026-08-28 23:15:18 +0500

    Student-management-project

--- Student-management-system/config/settings.py
+++ Student-management-system/config/settings.py
@@ -11,6 +11,17 @@ https://docs.djangoproject.com/en/6.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+DATABASES = {
+    'default': dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600,
+    )
+}
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
