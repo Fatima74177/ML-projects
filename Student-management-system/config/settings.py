a3cfca76b173844e248118ccd773865e3c5c5d2d
Fatima74177 <laibaak081@gmail.com>
2026-08-29 00:26:52 +0500

    Use Vercel Django detection

--- Student-management-system/config/settings.py
+++ Student-management-system/config/settings.py
@@ -34,7 +34,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-o)bf4kx-ncon5uyb#(#+s3bro7-ogz(^4i(wai5h=of7i*-e!3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
