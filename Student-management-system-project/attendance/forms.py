6ca160fb4c829b44e6225411b396c53279f6daf4
Fatima74177 <laibaak081@gmail.com>
2026-08-30 20:11:20 +0500

    Student-management-system-project

--- Student-management-system-project/attendance/forms.py
+++ Student-management-system-project/attendance/forms.py
@@ -7,3 +7,16 @@ class AttendanceForm(forms.ModelForm):
     class Meta:
         model = Attendance
         exclude = ['created_at']
+        widgets = {
+            'attendance_date': forms.DateInput(
+                attrs={'type': 'date', 'class': 'form-control'},
+                format='%Y-%m-%d',
+            ),
+        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['attendance_date'].input_formats = ['%Y-%m-%d']
+        for field in self.fields.values():
+            css_class = 'form-select' if isinstance(field.widget, forms.Select) else 'form-control'
+            field.widget.attrs.setdefault('class', css_class)
--- Student-management-system-project/students/forms.py
+++ Student-management-system-project/students/forms.py
@@ -7,3 +7,16 @@ class StudentForm(forms.ModelForm):
     class Meta:
         model = Student
         exclude = ['created_at']
+        widgets = {
+            'date_of_birth': forms.DateInput(
+                attrs={'type': 'date', 'class': 'form-control'},
+                format='%Y-%m-%d',
+            ),
+        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['date_of_birth'].input_formats = ['%Y-%m-%d']
+        for field in self.fields.values():
+            css_class = 'form-select' if isinstance(field.widget, forms.Select) else 'form-control'
+            field.widget.attrs.setdefault('class', css_class)
