3222f6b26e38f3b1e8d54503031825d342d13c57
Fatima74177 <laibaak081@gmail.com>
2026-08-30 20:41:40 +0500

    Student-management-system-project

--- Student-management-system-project/attendance/views.py
+++ Student-management-system-project/attendance/views.py
@@ -3,6 +3,7 @@ from django.urls import reverse_lazy
 from django.views.generic import CreateView, DeleteView, DetailView, ListView, UpdateView
 
 from accounts.mixins import RoleRequiredMixin, TeacherCourseScopedMixin
+from accounts.signals import synchronize_profile_role
 from .forms import AttendanceForm
 from .models import Attendance
 
@@ -12,6 +13,13 @@ class AttendanceListView(LoginRequiredMixin, ListView):
     template_name = 'attendance/attendance_list.html'
     context_object_name = 'attendance_records'
     ordering = ['-attendance_date']
+
+    def dispatch(self, request, *args, **kwargs):
+        # Update older teacher accounts before the navigation and attendance
+        # action buttons are rendered.
+        if request.user.is_authenticated:
+            synchronize_profile_role(request.user)
+        return super().dispatch(request, *args, **kwargs)
 
     def get_queryset(self):
         queryset = super().get_queryset().select_related('student', 'course', 'course__teacher')
@@ -26,6 +34,11 @@ class AttendanceListView(LoginRequiredMixin, ListView):
 class AttendanceDetailView(LoginRequiredMixin, DetailView):
     model = Attendance
     template_name = 'attendance/attendance_detail.html'
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            synchronize_profile_role(request.user)
+        return super().dispatch(request, *args, **kwargs)
 
     def get_queryset(self):
         queryset = super().get_queryset().select_related('student', 'course', 'course__teacher')
